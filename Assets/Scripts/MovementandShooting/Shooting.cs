42565ed58d8fc348575fe8a7072e2e80b594b7c1
Yuzuu17 <alvinnicoleeee@gmail.com>
2024-10-18 18:17:40 +0800

    Movement System
    -Implemented Movement System

---
+++ Assets/Scripts/MovementandShooting/Shooting.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float Force;
+    public float moveSpeed = 5f;                    
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = 0;
+
+                                                                
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveHorizontal = -moveSpeed;                                    // Move left
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveHorizontal = moveSpeed;                                     // Move right
+        }
+
+                                                                           // Apply movement
+        Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+        rb.MovePosition(rb.position + movement * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+        rb.AddForce(Vector3.back * Force, ForceMode.Impulse);
+    }
+}
