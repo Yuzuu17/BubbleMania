b6062c26588799c78ce2fe49417f97a1a7572484
Yuzuu17 <alvinnicoleeee@gmail.com>
2024-10-20 16:31:50 +0800

    Spawn And Randomizer Of Balls
    - Implemented Randomizer System
    - Implemented Spawn System

---
+++ Assets/Scripts/SpawnBall/SpawnBall.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+    public GameObject[] BallsPrefabs; // Array to hold your prefabs
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Randomly select one of the prefabs from the array
+            int randomIndex = Random.Range(0, BallsPrefabs.Length);
+            GameObject selectedPrefab = BallsPrefabs[randomIndex];
+
+            // Instantiate the selected prefab
+            Instantiate(selectedPrefab, transform.position, Quaternion.identity);
+            Debug.Log("Ball");
+        }
+    }
+}
